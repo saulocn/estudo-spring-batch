78c7279f4db6b1638f27b288c8a70029df3b9c6b
Saulo de Carvalho Neto <sacarvalho@uolinc.com>
2020-05-09 12:14:16 -0300

    Alteração para uso das configs do XML ao invés das classes

--- src/main/java/br/com/saulocn/estudo/springbatch/App.java
+++ src/main/java/br/com/saulocn/estudo/springbatch/App.java
@@ -1,20 +1,21 @@
 package br.com.saulocn.estudo.springbatch;
 
-import br.com.saulocn.estudo.springbatch.config.SpringBatchConfig;
-import br.com.saulocn.estudo.springbatch.config.SpringConfig;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         // Spring Java config
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(SpringConfig.class);
-        context.register(SpringBatchConfig.class);
-        context.refresh();
+        //        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        //        context.register(SpringConfig.class);
+        //        context.register(SpringBatchConfig.class);
+        //        context.refresh();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-batch-config.xml");
 
         JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
         Job job = (Job) context.getBean("firstBatchJob");
